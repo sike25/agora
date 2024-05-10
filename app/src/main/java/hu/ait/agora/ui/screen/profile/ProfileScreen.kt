51fba4513ec31f1e47ee903a134a5249fe9e8531
sike25 <oogieva25@amherst.edu>
2024-05-11 00:17:27 +0200

    fix:  refactored profile load into view model

--- app/src/main/java/hu/ait/agora/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/hu/ait/agora/ui/screen/profile/ProfileScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.google.firebase.auth.FirebaseAuth
@@ -44,58 +45,11 @@ import hu.ait.agora.ui.utils.AgoraBottomNavBar
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(
-    navController: NavController
+    navController: NavController,
+    profileScreenViewModel: ProfileScreenViewModel = viewModel()
 ) {
 
-    var user by remember {
-        mutableStateOf(
-            User(
-                profilePicture = "https://firebasestorage.googleapis.com/v0/b/agora-hills.appspot.com/o/ambika_mod.jpg?alt=media&token=d3df85eb-f7f1-41f7-9ced-ee79b944fcc6",
-                name = "Ambika Mod",
-                email = "Ambika is here because there was an error.",
-                purchaseHistory = emptyList(),
-                firebaseUID = "",
-                listedItems = emptyList()
-            )
-        )
-    }
-
-
-    try {
-        val currentUser = FirebaseAuth.getInstance().currentUser
-
-        if (currentUser != null) {
-            val userCollection = FirebaseFirestore.getInstance().collection("users")
-            val userRef = userCollection.document(currentUser.uid)
-            userRef.get().addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    val document = task.result
-                    if (document != null) {
-                        user = User(
-                                profilePicture = "https://firebasestorage.googleapis.com/v0/b/agora-hills.appspot.com/o/gump.jpg?alt=media&token=70dd2413-5201-4922-8b49-672af2912683",
-                                name = document.get("name") as String,
-                                email = document.get("email") as String,
-                                purchaseHistory = document.get("purchaseHistory") as List<String>? ?: emptyList(),
-                                firebaseUID = document.get("firebaseUID") as String,
-                                listedItems = document.get("listedItems") as List<String>? ?: emptyList(),
-                        )
-                    } else {
-                        throw IllegalStateException("No such document")
-                    }
-                } else {
-                    throw IllegalStateException("get failed with " + task.exception)
-                }
-            }
-        } else {
-            throw IllegalStateException("User not logged in")
-        }
-    } catch (e: Exception) {
-        Log.d("MY_TEST", "Error: ${e.message}")
-    }
-
-
-
-
+    val user = profileScreenViewModel.getThisUser()
 
     Scaffold(
         topBar = {
